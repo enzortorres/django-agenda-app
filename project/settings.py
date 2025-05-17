8eefe5886efc1eac2d31f50f254cc99050922002
enzortorres <enzortorres2005@gmail.com>
2025-05-17 15:25:46 -0300

    corrigindo detalhe

--- project/settings.py
+++ project/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
 STATIC_ROOT = BASE_DIR / 'static' # collect static
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
